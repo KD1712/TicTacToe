fea6c253cce05039d847ded2892d8a55785c6662
Khanjan <17khanjan@gmail.com>
2022-10-19 23:26:17 +0530

    Components modified added helpers

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,44 @@
 import { useState } from "react";
 import Board from "./Board";
+import { calculateWinner } from "../helpers";
+const styles = {
+  width: '200px',
+  margin: '20px auto',
+};
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
-  const handleClick = () => {};
+  const [XisNext, setXisNext] = useState(true);
+  const winner = calculateWinner(board);
 
-  const jumpTo = () => {};
+  const handleClick = (i) => {
+    const boardCopy = [...board];
 
-  const renderMoves = () => {};
+    if (winner || boardCopy[i]) return;
 
-  return <Board onClick={handleClick} />;
+    boardCopy[i] = XisNext ? "X" : "O";
+    setBoard(boardCopy);
+    setXisNext(!XisNext);
+  };
+
+  // const jumpTo = () => {};
+
+
+  const renderMoves = () => (
+    <button onClick={() => setBoard(Array(9).fill(null))}>
+        Start Game
+    </button>
+)
+
+
+  return (
+    <>
+        <Board squares={board} onClick={handleClick} />
+        <div style={styles}>
+            <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (XisNext ? 'X' : 'O')}</p>
+            {renderMoves()}
+        </div>
+    </>
+)
 };
 
 export default Game;
