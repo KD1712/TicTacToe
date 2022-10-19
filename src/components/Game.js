e0acd902a45b53bf021fc56dec7dbfbd699a0f6c
Khanjan <17khanjan@gmail.com>
2022-10-19 18:52:16 +0530

    Commit components(board,square,game)

---
+++ src/components/Game.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Board from "./Board";
+const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const handleClick = () => {};
+
+  const jumpTo = () => {};
+
+  const renderMoves = () => {};
+
+  return <Board onClick={handleClick} />;
+};
+
+export default Game;
